eeb522125140838616f020041a82275b0cee4f44
Chase Deanda <chasedeanda@gmail.com>
2017-06-19 13:18:09 -0500

    Moved html into views directory

--- examples/angularjs/js/app.js
+++ examples/angularjs/js/app.js
@@ -4,7 +4,7 @@ angular.module('todomvc', ['ngRoute', 'ngResource'])
 
 		const routeConfig = {
 			controller: 'TodoCtrl',
-			templateUrl: 'todomvc-index.html',
+			templateUrl: 'views/todomvc-index.html',
             resolve: {
                 store: function (todoStorage) {
                     // Get the correct module (API or localStorage).
