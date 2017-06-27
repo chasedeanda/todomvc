df3e134c524e2de7d329c7eaafcabc56dba42934
Chase Deanda <chasedeanda@gmail.com>
2017-06-27 17:15:09 -0500

    Prune old files

--- examples/angularjs/js/components/index.js
+++
@@ -1,8 +0,0 @@
-import { react2angular } from 'react2angular';
-
-import 'vendors';
-
-import TodoComponent from 'components/TodoComponent';
-
-angular.module('todomvc')
-    .component('todoComponent', react2angular(TodoComponent));
